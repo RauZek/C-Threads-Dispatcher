9da4bcd949ef90a6dd221202278f023b722ed538
Raul Hristov <rauhristov883@gmail.com>
2025-11-10 20:05:35 +0200

    Removing old c_threads folder

--- c_threads/src/c_advanced_process/advanced_process.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ADVANCED_PROCESS_H_
-#define __ADVANCED_PROCESS_H_
-
-#include "base_thread.h"
-
-base_process_t* advanced_process_functions(void);
-
-#endif  // __ADVANCED_PROCESS_H__
--- c_threads/src/c_simple_process/simple_process.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SIMPLE_PROCESS_H__
-#define __SIMPLE_PROCESS_H__
-
-#include "base_thread.h"
-
-base_process_t* simple_process_functions(void);
-
-#endif  // __SIMPLE_PROCESS_H__
